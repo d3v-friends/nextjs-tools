12bc973f3e7076accb5e45dad90a6bd88c94e5e4
hi-d3v-friends <d3v.friends@gmail.com>
2025-03-18 13:15:02 +0900

    0.1.18 add fnCss.sum()

---
+++ dist/func/css/sum.js
@@ -0,0 +1,7 @@
+import concat from "../css/concat";
+import trim from "../css/trim";
+export default function (...str) {
+    let res = concat(...str);
+    return trim(res);
+}
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3VtLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vLi4vc3JjL2Z1bmMvY3NzL3N1bS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSxPQUFPLE1BQU0sTUFBTSxrQkFBa0IsQ0FBQztBQUN0QyxPQUFPLElBQUksTUFBTSxnQkFBZ0IsQ0FBQztBQUVsQyxNQUFNLENBQUMsT0FBTyxXQUFXLEdBQUcsR0FBYTtJQUN4QyxJQUFJLEdBQUcsR0FBRyxNQUFNLENBQUMsR0FBRyxHQUFHLENBQUMsQ0FBQztJQUN6QixPQUFPLElBQUksQ0FBQyxHQUFHLENBQUMsQ0FBQztBQUNsQixDQUFDIn0=
--- src/func/css/index.ts
+++ src/func/css/index.ts
@@ -1,7 +1,9 @@
 import concat from "@func/css/concat";
+import sum from "@func/css/sum";
 import trim from "@func/css/trim";
 
 export default {
 	concat,
 	trim,
+	sum,
 };
