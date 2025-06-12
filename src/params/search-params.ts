8ad2f20a7b2145deba484df1fdfee538f2356781
ciaolee <ciaolee@d3v-friends.com>
2025-06-12 17:15:51 +0900

    0.1.60

--- src/params/search-params.ts
+++ src/params/search-params.ts
@@ -1,5 +1,8 @@
 "use client";
-export default function searchParams(params: Record<string, string | number | null>, host?: string): string {
+export default function searchParams(
+	params: Record<string, string | number | null | undefined>,
+	host?: string
+): string {
 	const u = new URLSearchParams(window.location.search);
 	for (const key in params) {
 		if (params[key]) {
