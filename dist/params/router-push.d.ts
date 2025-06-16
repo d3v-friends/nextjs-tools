917f3bdeb613eaa912636007f75ba768375bacbd
ciaolee <ciaolee@d3v-friends.com>
2025-06-16 11:58:21 +0900

    0.1.65

--- dist/params/router-push.d.ts
+++ dist/params/router-push.d.ts
@@ -1,1 +1,2 @@
-export default function (params: Record<string, string | number | null | undefined>, host?: string): void;
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
+export default function (router: AppRouterInstance, params: Record<string, string | number | null | undefined>, host?: string): void;
--- src/params/router-push.ts
+++ src/params/router-push.ts
@@ -1,10 +1,12 @@
 "use client";
 
-import {useRouter} from "next/navigation";
+import {AppRouterInstance} from "next/dist/shared/lib/app-router-context.shared-runtime";
 
-export default function (params: Record<string, string | number | null | undefined>, host?: string) {
-	const router = useRouter();
-
+export default function (
+	router: AppRouterInstance,
+	params: Record<string, string | number | null | undefined>,
+	host?: string
+) {
 	const u = new URLSearchParams(window.location.search);
 	for (const key in params) {
 		if (params[key]) {
@@ -15,6 +17,5 @@ export default function (params: Record<string, string | number | null | undefin
 	}
 
 	host = host || window.location.pathname;
-
 	router.push(`${host}?${u.toString()}`);
 }
