e131ecd5fdf3b4f7dd05348a41a77a5835a13b59
ciaolee <ciaolee@d3v-friends.com>
2025-07-03 18:06:57 +0900

    1.0.5

--- src/types/utillity.ts
+++ src/types/utillity.ts
@@ -2,6 +2,7 @@ import {StaticImageData} from "next/image";
 import {ReactNode} from "react";
 
 export type Nullable<T> = T | null | undefined;
+export type Undefined<T> = T | undefined;
 export type ArrayElement<ArrayType extends readonly unknown[]> = ArrayType extends readonly (infer ElementType)[]
 	? ElementType
 	: never;
